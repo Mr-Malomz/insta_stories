939f164c904c12c7d9b009b073942da8b818f622
demola malomo <malomo.alaba@yahoo.com>
2022-01-18 21:29:01 +0100

    info and stories component

---
+++ components/Info.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="text-sm w-full lg:w-2/3">
+    <section class="flex items-center mb-4">
+      <h1 class="font-normal mr-6">Estelle_</h1>
+      <button
+        class="border border-gray-200 py-1 md:px-10 font-bold text-sm rounded-md"
+      >
+        Message
+      </button>
+    </section>
+    <div class="flex mb-4">
+      <div class="md:flex md:space-x-1 mr-8">
+        <p class="font-bold text-sm">13</p>
+        <p class="text-sm font-medium text-gray-400">posts</p>
+      </div>
+      <div class="md:flex md:space-x-1 mr-8">
+        <p class="font-bold text-sm">194</p>
+        <p class="text-sm font-medium text-gray-400">followers</p>
+      </div>
+      <div class="md:flex md:space-x-1 mr-8">
+        <p class="font-bold text-sm">182</p>
+        <p class="text-sm font-medium text-gray-400">following</p>
+      </div>
+    </div>
+    <p>
+      An online store 🏬 we deal on all kinds of footwear 👠👞 for both male and
+      female. Contact us ☎{" "}
+      <span class="text-blue-600">@08141703050</span>. Worldwide delivery 🌍
+      <span class="text-blue-600">@Owerri</span>, Nigeria
+    </p>
+    <p class="text-xs py-5">
+      followed by <span class="font-bold">dpkreativ</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,64 @@
 <template>
-  <Tutorial/>
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <div
+        class="flex flex-col lg:flex-row items-center mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6"
+      >
+        <div
+          class="w-44 h-44 flex items-center justify-center border-indigo-400 border-4 mr-4 rounded-full cursor-pointer"
+          role="button"
+          @click="toggleStory"
+        >
+          <cld-image
+            :public-id="images[0]"
+            class="rounded-full w-40 h-40"
+          ></cld-image>
+        </div>
+        <Info />
+      </div>
+    </div>
+    <div
+      v-if="showStory"
+      class="absolute h-screen w-screen bg-black bg-opacity-70 top-0 h-100vh"
+    >
+      <section class="flex justify-end p-4">
+        <button
+          @click="toggleStory"
+          class="px-4 py-2 rounded-lg shadow-md bg-indigo-700 font-semibold text-white capitalize"
+        >
+          close
+        </button>
+      </section>
+      <section class="flex items-center justify-center">
+        <Stories
+          v-if="showStory"
+          :stories="images"
+          class="absolute top-10 left h-4/5 w-2/4"
+        />
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import { Stories } from 'vue-insta-stories'
+import images from '@/utils/stories.json'
+
+export default {
+  components: { Stories },
+  data() {
+    return {
+      images,
+      showStory: false,
+    }
+  },
+
+  methods: {
+    toggleStory() {
+      this.showStory = !this.showStory
+    },
+  },
+}
 </script>
